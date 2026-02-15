4035ebfd8d209c228585ed94a62fed9d96fa363a
Fazal <fazalbkabeer@gmail.com>
2026-02-15 14:44:05 +0530

    implemented bubble sort

---
+++ algorithms/sorting/bubble.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 'while' style
+
+func BubbleSort(a []int) {
+	n := len(a)
+	swapped := true
+
+	for swapped {
+		swapped = false
+		for i := 0; i < n-1; i++ {
+			if a[i] > a[i+1] {
+				a[i], a[i+1] = a[i+1], a[i]
+				swapped = true
+			}
+		}
+		n--
+	}
+}
+
+// using range (idiomatic)
+
+// func BubbleSort(a []int) {
+// 	n := len(a)
+
+// 	for i := 0; i < n-1; i++ {
+// 		// We use a slice of the array to limit the range
+// 		for j := range a[:n-1-i] {
+// 			if a[j] > a[j+1] {
+// 				a[j], a[j+1] = a[j+1], a[j]
+// 			}
+// 		}
+// 	}
+// }
+
+// func BubbleSort(a []int) {
+// 	n := len(a)
+// 	for i := 0; i < n-1; i++ {
+// 		swapped := false
+// 		for j := 0; j < n-i-1; j++ {
+// 			if a[j] > a[j+1] {
+// 				a[j], a[j+1] = a[j+1], a[j]
+// 				swapped = true
+// 			}
+// 		}
+// 		if !swapped {
+// 			break
+// 		}
+// 	}
+// }
+
+func main() {
+
+	a := []int{4, 7, 1, 0, 8, 2, 6, 3}
+
+	BubbleSort(a)
+
+	fmt.Println(a)
+}
