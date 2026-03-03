b4102b1ec5613a121c42a9cc4ffa839b9b110282
Fazal <fazalbkabeer@gmail.com>
2026-03-03 11:53:05 +0530

    implimented 'quick sort' using go

--- algorithms/sorting/quick.go
+++ algorithms/sorting/quick.go
@@ -1,2 +1,36 @@
 package main
 
+import "fmt"
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+
+	return i + 1
+}
+
+func QuickSort(arr []int, low, high int) {
+	if low < high {
+		pivotIndex := partition(arr, low, high)
+
+		QuickSort(arr, low, pivotIndex-1)
+		QuickSort(arr, pivotIndex+1, high)
+	}
+}
+
+func main() {
+	arr := []int{8, 3, 1, 7, 0, 10, 2}
+
+	QuickSort(arr, 0, len(arr)-1)
+
+	fmt.Println(arr)
+}
